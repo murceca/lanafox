9a5c0dd34689ac6af1e1c457740b17a3cd01cdbf
Svitlana Grischuk <murceca@gmail.com>
2018-12-06 13:01:08 +0200

    Deleted redundant codes

--- server/public/javascripts/photos.js
+++ server/public/javascripts/photos.js
@@ -1,10 +1,3 @@
-$('.carousel').carousel()
-
-// MDB Lightbox Init
-$(function () {
-    $('#mdb-lightbox-ui').load('mdb-addons/mdb-lightbox-ui.html');
-});
-
 $(function () {
     baguetteBox.run('.tz-gallery');
 });
