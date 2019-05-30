fb73c03a6cc7d0eeabeb5c7c080bcf59294ad7b8
Svitlana Grischuk <murceca@gmail.com>
2019-05-30 15:22:06 +0300

    Code review

---
+++ server/public/javascripts/contact.js
@@ -0,0 +1,21 @@
+$(function () {
+  console.log('Found ' + $('.js-contact-form').length + ' contact forms.');
+  $('.js-contact-form').submit(function (event) {
+    event.preventDefault();
+    const $contactForm = $(this);
+    const nameVal = $contactForm.find('.js-name-field').val();
+    const emailVal = $contactForm.find('.js-email-field').val();
+    const messageVal = $contactForm.find('.js-message-field').val();
+    $.post('/contact', {
+      name: nameVal,
+      email: emailVal,
+      message: messageVal
+    })
+      .done(data => {
+        console.log('Done: ' + data);
+      })
+      .fail(error => {
+        console.log('Error: ' + error);
+      });
+  });
+});
--- server/routes/contact.js
+++ server/routes/contact.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('contact', { title: 'Contact' });
 });
 
+/* Contact form submit. */
+router.post('/', function(req, res, next) {
+  let emailSent = false;
+  res.json({
+    reqName: req.body.name,
+    reqEmail: req.body.email,
+    reqMessage: req.body.message,
+    success: emailSent
+  });
+});
+
 module.exports = router;
