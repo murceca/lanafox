1e019996e5147d3a6c42213d76a23b6e4b5d7cde
Svitlana Grischuk <murceca@gmail.com>
2020-08-04 21:16:10 +0300

    Photos optimization

--- server/configs.js
+++ server/configs.js
@@ -4,6 +4,8 @@ module.exports = {
   VIEWS_ABS_PATH: path.join(__dirname, 'views'),
   PARTIALS_ABS_PATH: path.join(__dirname, 'views', 'partials'),
   STATIC_ABS_PATH: path.join(__dirname, 'public'),
-  PHOTOS_REL_PATH: path.join('images', 'photos'),
-  PHOTOS_ABS_PATH: path.join(__dirname, 'public', 'images', 'photos'),
+  PHOTOS_REL_PATH: path.join('images', 'photos', 'hi-res'),
+  PHOTOS_ABS_PATH: path.join(__dirname, 'public', 'images', 'photos', 'hi-res'),
+  PREVIEWS_REL_PATH: path.join('images', 'photos', 'previews'),
+  PREVIEWS_ABS_PATH: path.join(__dirname, 'public', 'images', 'photos', 'previews')
 };
--- server/routes/photos.js
+++ server/routes/photos.js
@@ -4,16 +4,69 @@ const fs = require('fs');
 const junk = require('junk');
 const configs = require('../configs');
 
+class ImagesLoader {
+  getImages() {
+    const previewsList = this.previews;
+    const photosList = this.photos;
+    const imagesMap = [];
+
+    photosList.forEach(photo => {
+      const imageData = {
+        photo: this.buildFullPhotoUrl(photo),
+        altText: this.getAltText(photo)
+      };
+      const hasPreview = previewsList.indexOf(photo) !== -1;
+      if (hasPreview) {
+        imageData.preview = this.buildFullPreviewUrl(photo);
+      } else {
+        imageData.preview = imageData.photo;
+      }
+      imagesMap.push(imageData);
+    });
+    return imagesMap;
+  }
+
+  get previews() {
+    return fs.readdirSync(configs.PREVIEWS_ABS_PATH).filter(junk.not);
+  }
+
+  get photos() {
+    return fs.readdirSync(configs.PHOTOS_ABS_PATH).filter(junk.not);
+  }
+
+  buildFullPhotoUrl(photoFileName) {
+    return `${configs.PHOTOS_REL_PATH}/${photoFileName}`;
+  }
+
+  buildFullPreviewUrl(previewFileName) {
+    return `${configs.PREVIEWS_REL_PATH}/${previewFileName}`;
+  }
+
+  getAltText(photoFileName) {
+    // Remove extension
+    let altText = photoFileName.split('.');
+    altText.pop();
+
+    // Remove index (first part of the name usually)
+    altText = altText.join('');
+    altText = altText.split('_');
+    altText.shift();
+
+    // Replace underscore symbols with spaces
+    altText = altText.join(' ');
+    altText = altText.charAt(0).toUpperCase() + altText.slice(1);
+
+    return altText;
+  }
+}
+
 /* GET photos page. */
 router.get('/', function(req, res, next) {
-  const photos = fs.readdirSync(configs.PHOTOS_ABS_PATH)
-    .filter(junk.not)
-    .map(photoFileName => {
-      return `/${configs.PHOTOS_REL_PATH}/${photoFileName}`;
-    });
+  const imagesLoader = new ImagesLoader();
+  const photos = imagesLoader.getImages();
   res.render('photos', {
     title: 'Photos',
-    photos: photos || []
+    photos: photos
   });
 });
 
