fc92545dec5b9cbd984617396ee0c8321a19d2e0
Svitlana <r.svetulik@gmail.com>
2020-12-16 02:23:54 +0200

    Showing a link to the repo for curious users

---
+++ app/client/javascripts/common.js
@@ -0,0 +1,2 @@
+ // Showing a link to the repo for curious users
+ console.log('I see you are very curious about this site! If you wanna see all of the code, visit this link: https://github.com/murceca/lanafox');
