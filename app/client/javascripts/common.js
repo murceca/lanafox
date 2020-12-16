0b01b662c425442d002f60527d6549b5f2899c67
Svitlana <r.svetulik@gmail.com>
2020-12-16 20:26:12 +0200

    Typo

--- app/client/javascripts/common.js
+++ app/client/javascripts/common.js
@@ -1,2 +1,2 @@
  // Showing a link to the repo for curious users
- console.log('I see you are very curious about this site! If you wanna see all of the code, visit this link: https://github.com/murceca/lanafox');
+ console.log('I see you are very curious about this website! If you would like to see all of the code, go by the following link: https://github.com/murceca/lanafox');
